4ee51c761d001986f5c92930cf2a478d98a3101e
jps <j263792@dav.unicamp.br>
2022-08-11 00:49:21 -0300

    intances generator created for creating instances with one cloudlet and N VMs

--- greedy_vs_exact/instances/data_gen_2.py
+++ greedy_vs_exact/instances/data_gen_2.py
@@ -5,24 +5,65 @@ from itertools import product
 
 def vmGen(vmsQtt):
     VMs = []
-    for i in range(1, vmsQtt+1):
-        # units: storage(MB), cpu(MIPS), RAM(MB)
-        simStorage = 512 * 1024
-        simCPU = 2000
-        simRAM = 64
-        VMs.append({
-                "id": f"v{i}",
-                "bid": random.randint(60, 100),
-                "v_storage": random.randint(int(0.5*simStorage), simStorage), 
-                "v_CPU": random.randint(int(0.5*simCPU), simCPU), 
-                "v_RAM": random.randint(int(0.5*simRAM), simRAM), 
-            })
+    # units: storage(MB), cpu(MIPS), RAM(MB)
+    simStorage = 512 * 1024
+    simCPU = 2000
+    simRAM = 64
+
+    for i in range(1, int((vmsQtt)/2)):
+        groupA = {  "id": f"v{i}",
+                    "bid": random.randint(80, 120),
+                    "v_storage": random.randint(int(0.9*(0.5*simStorage)), int(1.1*(0.5*simStorage))), 
+                    "v_CPU": random.randint(int(0.9*(0.5*simCPU)), int(1.1*(0.5*simCPU))), 
+                    "v_RAM": random.randint(int(0.9*(0.5*simRAM)), int(1.1*(0.5*simRAM)))
+                 }
+
+        groupB = {  "id": f"v{i}",
+                    "bid": random.randint(80, 120),
+                    "v_storage": random.randint(int(0.9*simStorage), int(1.1*simStorage)), 
+                    "v_CPU": random.randint(int(0.9*simCPU), int(1.1*simCPU)), 
+                    "v_RAM": random.randint(int(0.9*simRAM), int(1.1*simRAM))
+                 }
+
+        groupC = {  "id": f"v{i}",
+                    "bid": random.randint(80, 120),
+                    "v_storage": random.randint(int(0.9*(1.5*simStorage)), int(1.1*(1.5*simStorage))), 
+                    "v_CPU": random.randint(int(0.9*(1.5*simCPU)), int(1.1*(1.5*simCPU))), 
+                    "v_RAM": random.randint(int(0.9*(1.5*simRAM)), int(1.1*(1.5*simRAM)))
+                 }
+
+        VMs.append(groupB)
+
+    for i in range(int((vmsQtt)/2), vmsQtt+1):
+        groupA = {  "id": f"v{i}",
+                    "bid": random.randint(80, 120),
+                    "v_storage": random.randint(int(0.9*(0.5*simStorage)), int(1.1*(0.5*simStorage))), 
+                    "v_CPU": random.randint(int(0.9*(0.5*simCPU)), int(1.1*(0.5*simCPU))), 
+                    "v_RAM": random.randint(int(0.9*(0.5*simRAM)), int(1.1*(0.5*simRAM)))
+                }
+
+        groupB = {  "id": f"v{i}",
+                    "bid": random.randint(80, 120),
+                    "v_storage": random.randint(int(0.9*simStorage), int(1.1*simStorage)), 
+                    "v_CPU": random.randint(int(0.9*simCPU), int(1.1*simCPU)), 
+                    "v_RAM": random.randint(int(0.9*simRAM), int(1.1*simRAM))
+                }
+
+        groupC = {  "id": f"v{i}",
+                    "bid": random.randint(80, 120),
+                    "v_storage": random.randint(int(0.9*(1.5*simStorage)), int(1.1*(1.5*simStorage))), 
+                    "v_CPU": random.randint(int(0.9*(1.5*simCPU)), int(1.1*(1.5*simCPU))), 
+                    "v_RAM": random.randint(int(0.9*(1.5*simRAM)), int(1.1*(1.5*simRAM)))
+                }
+
+        VMs.append(groupC)
     
     return VMs
 
 def cloudletGen(cloudletQtt):
     Cloudlets = []
     # units: storage(MB), cpu(MIPS), RAM(MB)
+
     Cloudlets.append({
                 "id": "c1",
                 "c_storage": 16 * 1024 * 1024, 
