1d04315c12e6c8253378d10479c456b9d61c426f
jps <j263792@dav.unicamp.br>
2023-01-23 11:36:48 +0100

    new instances to test if our algorithm is better than the paper's one

--- greedy_vs_exact/instances/data_gen.sh
+++ greedy_vs_exact/instances/data_gen.sh
@@ -12,5 +12,5 @@ seeds=(173 978 659 293 176 392 549 633 798 327
 for i in {0..99}
 do
     fileNumber=$i
-    python3 data_gen_mult.py 200 20 /home/jps/allocation_models/greedy_vs_exact/instances/vTheta/MMD_2/inst_${fileNumber}.json ${seeds[i]}
+    python3 data_gen_mult.py 200 20 /home/jps/allocation_models/greedy_vs_exact/instances/vIota/500_50/inst_${fileNumber}.json ${seeds[i]}
 done
--- greedy_vs_exact/instances/data_gen_mult.py
+++ greedy_vs_exact/instances/data_gen_mult.py
@@ -7,41 +7,41 @@ def vmGen(vmsQtt):
     VMs = []
     # units: storage(MB), cpu(MIPS), RAM(MB)
     simMIPS = 2000
-    for i in range(0, 50):
+    for i in range(0, 125):
         VMs.append({
                 "id": f'v{i}', 
                 "vmType": 'gp1',
                 "bid": int(random.gauss(100, 10)),
-                "v_storage": 3 * 1024, 
-                "v_CPU": 2 * simMIPS, 
-                "v_RAM": 4 * 1024
+                "v_storage": int(random.gauss(3 * 1024, 300)), 
+                "v_CPU": int(random.gauss(2 * simMIPS, 400)), 
+                "v_RAM": int(random.gauss(4 * 1024, 400))
             })
-    for j in range(50, 100):
+    for j in range(125, 250):
         VMs.append({  
                 "id": f'v{j}',  
                 "vmType": 'gp2',
                 "bid": int(random.gauss(100, 10)),
-                "v_storage": 16 * 1024, 
-                "v_CPU": 4 * simMIPS, 
-                "v_RAM": 16 * 1024
+                "v_storage": int(random.gauss(16 * 1024, 1600)), 
+                "v_CPU": int(random.gauss(4 * simMIPS, 800)), 
+                "v_RAM": int(random.gauss(16 * 1024, 1600))
             })
-    for k in range(100, 150):
+    for k in range(250, 375):
         VMs.append({
                 "id": f'v{k}',
                 "vmType": 'ramInt',
                 "bid": int(random.gauss(150, 10)),
-                "v_storage": 16 * 1024, 
-                "v_CPU": 8 * simMIPS, 
-                "v_RAM": 64 * 1024
+                "v_storage": int(random.gauss(16 * 1024, 1600)), 
+                "v_CPU": int(random.gauss(8 * simMIPS, 800)), 
+                "v_RAM": int(random.gauss(64 * 1024, 6400))
             })
-    for l in range(150, 200):
+    for l in range(375, 500):
         VMs.append({
                 "id": f'v{l}', 
                 "vmType": 'cpuInt',
                 "bid": int(random.gauss(150, 10)),
-                "v_storage": 16 * 1024, 
-                "v_CPU": 16 * simMIPS, 
-                "v_RAM": 32 * 1024
+                "v_storage": int(random.gauss(16 * 1024, 1600)), 
+                "v_CPU": int(random.gauss(16 * simMIPS, 3200)), 
+                "v_RAM": int(random.gauss(32 * 1024, 3200))
             })
     return VMs
 
@@ -50,39 +50,39 @@ def cloudletGen(cloudletQtt):
     simMIPS = 2000
     # units: storage(MB), cpu(MIPS), RAM(MB)
 
-    for i in range(0, 2):
-        Cloudlets.append({ "id": f"cA_{i}",
-            "c_storage": 250 * 1024, 
-            "c_CPU": 12 * simMIPS,
-            "c_RAM": 16 * 1024
-            })
+    # for i in range(0, 2):
+    #     Cloudlets.append({ "id": f"cA_{i}",
+    #         "c_storage": 250 * 1024, 
+    #         "c_CPU": 12 * simMIPS,
+    #         "c_RAM": 16 * 1024
+    #         })
     
-    for j in range(2, 4):
-        Cloudlets.append({ "id": f"cB_{j}",
-                "c_storage": 500 * 1024, 
-                "c_CPU": 16 * simMIPS,
-                "c_RAM": 32 * 1024
-            })
+    # for j in range(2, 4):
+    #     Cloudlets.append({ "id": f"cB_{j}",
+    #             "c_storage": 500 * 1024, 
+    #             "c_CPU": 16 * simMIPS,
+    #             "c_RAM": 32 * 1024
+    #         })
     
-    for k in range(4, 6):
-        Cloudlets.append({ "id": f"cC_{k}",
-                "c_storage": 1000 * 1024, 
-                "c_CPU": 22 * simMIPS,
-                "c_RAM": 64 * 1024
-            })
+    # for k in range(4, 6):
+    #     Cloudlets.append({ "id": f"cC_{k}",
+    #             "c_storage": 1000 * 1024, 
+    #             "c_CPU": 22 * simMIPS,
+    #             "c_RAM": 64 * 1024
+    #         })
 
-    for l in range(6, 8):
-        Cloudlets.append({ "id": f"cD_{l}",
-                "c_storage": 2 * 1000 * 1024, 
-                "c_CPU": 32 * simMIPS,
-                "c_RAM": 256 * 1024
-            })
+    # for l in range(6, 8):
+    #     Cloudlets.append({ "id": f"cD_{l}",
+    #             "c_storage": 2 * 1000 * 1024, 
+    #             "c_CPU": 32 * simMIPS,
+    #             "c_RAM": 256 * 1024
+    #         })
 
-    for m in range(6, 8):
+    for m in range(50):
         Cloudlets.append({ "id": f"cE_{m}",
-                "c_storage": 4 * 1000 * 1024, 
-                "c_CPU": 60 * simMIPS,
-                "c_RAM": 512 * 1024
+                "c_storage": 4 * 2 * 1000 * 1024, 
+                "c_CPU": 60 * 2 * simMIPS,
+                "c_RAM": 512 * 2 * 1024
             })
 
     return Cloudlets
